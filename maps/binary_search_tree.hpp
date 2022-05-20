f5b78686ea3b85d95de1082790d5d6346b24cc44
ricardomart26 <ricardomartins2626t@gmail.com>
2022-05-20 23:35:23 +0100

    .

--- maps/binary_search_tree.hpp
+++ maps/binary_search_tree.hpp
@@ -2,6 +2,8 @@
 #define BINARY_SEARCH_TREE
 
 #include <utility>
+#include <string>
+#include <iostream>
 
 class bst {
     
@@ -9,53 +11,113 @@ class bst {
 
         typedef std::pair<std::string, int> value_type;
         
+        typedef struct s_node
+        {
+            value_type _data;
+            struct s_node *_left;
+            struct s_node *_right;
+        } t_node;
+
         bst()
         {
-            head = this;
-            head = new bst;
+            head = new t_node;
             head->_data = std::make_pair("random", 3);
             head->_left = NULL;
             head->_right = NULL;
         }
 
-        bool    compare(const value_type& elem, const value_type& toAdd)
+        bool    compare(const std::pair<std::string, int>& elem, const std::pair<std::string, int>& toAdd)
         {
-            return (elem.first() < toAdd.first());
+            // random vs random2
+            return (elem.first < toAdd.first);
         }
 
-        ~bst();
+        ~bst() {};
 
-        void    add_node(const value_type& type)
+        void    add_node(t_node *node, const std::pair<std::string, int>& type)
         {
-            static bst *temp = head;
-            if (compare(temp->_data, type)) {
-                if (temp->_left == NULL) {
-                    temp->_left = new bst;
-                    temp->_left->_data = type;
-                    return ;
+            static int counter = 0;
+            if (!compare(node->_data, type)) {
+                // std::cout << "smaller\n";
+
+                if (node->_left == NULL) {
+                    node->_left = new t_node;
+                    node->_left->_data = type;
+                    // std::cout << counter++ << ": " << node->_left->_data.first << " (left) " << std::endl;
                 } else {
-                    temp = _left;
-                    add_node(type);
+                    // std::cout << "(left) no add\n";
+                    add_node(node->_left, type);
                 }
             } else {
-                if (_right == NULL) {
-                    temp->_right = new bst;
-                    temp->_right->_data = type;
-                    return ;
+                // std::cout << "bigger\n";
+                if (node->_right == NULL) {
+                    node->_right = new t_node;
+                    node->_right->_data = type;
+                    // std::cout << counter++ << ": " << node->_right->_data.first << " (right) " << std::endl;
                 } else {
-                    temp = _right;
-                    add_node(type);
+                    // std::cout << "(right) no add\n";
+                    add_node(node->_right, type);
                 }
             }
-            return ;
         }
-    
+
+        int find_width(t_node *node, int w)
+        {
+            static int ret = 0;
+            int save_ret;
+
+            if (w > ret)
+                ret = w;
+            if (node->_left != NULL)
+                if ((save_ret = find_width(node->_left, ++w)) > ret)
+                    ret = save_ret;
+            if (node->_right != NULL)
+                if ((save_ret = find_width(node->_right, ++w)) > ret)
+                    ret = save_ret;
+
+            if (node == head)
+                return (ret - 1);
+            return (w);
+        }
+
+        void    print_bst() {
+            static int nv = 1;
+            static t_node *my_node = head;
+            int i = 0;
+            int width = find_width(head, 0);
+            std::cout << "width: " << width << std::endl;
+            return;
+            if (nv == 1) {
+                std::cout << "\t\t" << head->_data.first << "\t\t\n";
+            }
+            static t_node *myLeft = head->_left;
+            static t_node *myRight = head->_right;
+            std::cout << "Chegou aqui!\n";
+            while (i < nv)
+            {
+                if (myLeft != NULL)
+                    std::cout << "\t" << myLeft->_data.first << "\t";
+                if (myRight != NULL)
+                    std::cout << "\t" << myRight->_data.first << "\t";
+                i++;
+                if (i == nv)
+                    std::cout << "\n";
+            }
+            nv++;
+            my_node = myLeft;
+            if (!myLeft && !myRight)
+                return ;
+            print_bst();
+        }
+
+        t_node *getHead() const
+        {
+            return (head);
+        }
+
     private:
 
-        value_type _data;
-        bst *head;
-        bst *_left;
-        bst *_right;
+        t_node *head;
 };
 
 #endif
--- maps/main.cpp
+++ maps/main.cpp
@@ -2,5 +2,18 @@
 
 int main(void) {
     bst tree;
+
+    std::cout << "../\n";
+    tree.add_node(tree.getHead(), std::make_pair("random2", 4));
+    tree.add_node(tree.getHead(), std::make_pair("rando", 5));
+    tree.add_node(tree.getHead(), std::make_pair("rand", 3));
+    tree.add_node(tree.getHead(), std::make_pair("ran", 2));
+    // tree.add_node(tree.getHead(), std::make_pair("ra", 1));
+    // tree.add_node(tree.getHead(), std::make_pair("r", 0));
+    tree.add_node(tree.getHead(), std::make_pair("random22", 6));
+    // tree.add_node(tree.getHead(), std::make_pair("random222", 7));
+    tree.print_bst();
+    if (std::string("teste") < std::string("teste2"))
+        std::cout << "E menor amigos!\n";
     return (0);
 }
